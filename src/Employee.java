3fab03420c235d09639d9097479c32c13cb8a440
Santhosh <Santhosh@KavithaJukanti>
2021-06-10 14:45:16 +0530

    First commit

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+
+public class Employee {
+	
+	String name;
+	int empid;
+	Employee(String name,int empid){
+	this.name = name;
+	this.empid = empid;
+
+}
+
+	public static void main(String[]args){
+
+	Employee e1 = new Employee("Kavitha",101);
+		
+	}
+
+}
